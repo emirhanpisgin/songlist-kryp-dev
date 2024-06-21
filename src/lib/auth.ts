03a23e515a4e82dab59537a95c0356993fb0fe36
kryparnold <emirhanpgt@gmail.com>
2024-06-21 15:25:37 +0300

    implemented drizzle-orm with authjs and three providers

---
+++ src/lib/auth.ts
@@ -0,0 +1,11 @@
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import NextAuth from "next-auth";
+import { db } from "./db";
+import Google from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
+import Discord from "next-auth/providers/discord";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+	adapter: DrizzleAdapter(db),
+	providers: [Google, Github, Discord],
+});
