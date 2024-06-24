d237885061d2f23f8d37771826834e5ad920df7a
kryparnold <emirhanpgt@gmail.com>
2024-06-24 14:45:24 +0300

    styling changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -63,10 +63,15 @@ const config = {
 					from: { height: "var(--radix-accordion-content-height)" },
 					to: { height: "0" },
 				},
+                "slide-in": {
+                    from: { opacity: "0", transform: "translateY(-0.5rem)"},
+                    to: { opacity: "1", transform: "translateY(0)"}
+                }
 			},
 			animation: {
 				"accordion-down": "accordion-down 0.2s ease-out",
 				"accordion-up": "accordion-up 0.2s ease-out",
+                "slide-in": "slide-in 0.4s forwards var(--slide-in-delay,0ms)"
 			},
 			fontFamily: {
 				sans: ["var(--font-sans)", ...fontFamily.sans],
