56aada54a26670b01c3d4ab89cabd40661c6708c
kryparnold <emirhanpgt@gmail.com>
2024-06-24 18:02:37 +0300

    util changes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,22 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
+}
+
+export function timePassed(date: Date): string {
+	const now = new Date();
+	const diff = Math.abs(now.getTime() - date.getTime());
+
+	const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+	if (days > 0) return `${days}g`;
+
+	const hours = Math.floor(diff / (1000 * 60 * 60));
+	if (hours > 0) return `${hours}s`;
+
+	const minutes = Math.floor(diff / (1000 * 60));
+	if (minutes > 0) return `${minutes}d`;
+
+	return "Şimdi";
 }
